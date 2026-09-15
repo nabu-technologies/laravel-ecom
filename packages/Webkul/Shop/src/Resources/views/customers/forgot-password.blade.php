b79890f0a210be3110e4b660b8c10b86e50674a6
vinita978 <vinitakumari504931@gmail.com>
2026-09-15 16:46:33 +0530

    update: adjust margin to padding for container in forgot and reset password views

--- packages/Webkul/Shop/src/Resources/views/customers/forgot-password.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/forgot-password.blade.php
@@ -15,7 +15,7 @@
         @lang('shop::app.customers.forgot-password.title')
     </x-slot>
 
-    <div class="container mt-20 max-1180:px-5 max-md:mt-12">
+    <div class="container pt-10 max-1180:px-5 max-md:mt-12">
         {!! view_render_event('bagisto.shop.customers.forget_password.logo.before') !!}
 
         <!-- Company Logo -->
--- packages/Webkul/Shop/src/Resources/views/customers/reset-password.blade.php
+++ packages/Webkul/Shop/src/Resources/views/customers/reset-password.blade.php
@@ -18,7 +18,7 @@
         @lang('shop::app.customers.reset-password.title')
         </x-slot>
 
-        <div class="container mt-20 max-1180:px-5 max-md:mt-12">
+        <div class="container pt-10 max-1180:px-5 max-md:mt-12">
             {!! view_render_event('bagisto.shop.customers.reset_password.logo.before') !!}
 
             <!-- Company Logo -->
